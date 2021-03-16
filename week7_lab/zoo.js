d0bec6e1b16da07997f0815065d9858287e95464
Noni Shelton <nonishelton@nonis-mbp.wifi.local.cmu.edu>
2021-03-16 14:18:45 -0400

    should be updated

--- week7_lab/zoo.js
+++ week7_lab/zoo.js
@@ -18,9 +18,30 @@ function zebra(name, age) {
 }
 
 let animals = (new redPanda(), new elephant(), new zebra());
-let names ["patiria","anaya","javon", "lucy","paul"];
+let names = ["patiria","anaya","javon", "lucy","paul"];
 
 function generateRandomIndex(maxIndex){
 	return Math.floor(Math.random()*maxIndex);
 }
+function generateRandomName(){
+	let randomIndex = names[generateRandomIndex(maxIndex)];
+}
+function generateRandomAge(){
+	let randomAge = generateRandomIndex(9);
+}
+function generateRandomAnimal(){
+	let randomIdx = animals[generateRandomIndex(maxIndex)];
+	if (randomAnimal instanceof elephant){
+		return new elephant(generateRandomName(), generateRandomAge());
+	}
+	else if (randomAnimal instanceof redPanda){
+		return new redPanda(generateRandomName(), generateRandomAge());
+	}
+	else if (randomAnimal instanceof zebra){
+		return new zebra(generateRandomName(), generateRandomAge());
+	}
+function onLoad() {
+	var animal = generateRandomAnimal();
+}
 
+}
